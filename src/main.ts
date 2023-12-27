6d8aae1da2ec801c3d6f960526f392f84f61d0c1
Sebastian L. K. Sorensen <sebastian.sorensen@public.com>
2023-12-27 20:53:30 +0100

    fix: add source files

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import {type Component, mount, tags} from '@tentjs/tent'
+
+const {div, button} = tags
+
+type State = {
+  count: number
+}
+
+const Counter: Component<State> = {
+  state: {count: 0},
+  view: ({state}) => div([
+    button('+', {onclick: () => state.count++}),
+    div(`${state.count}`),
+    button('-', {onclick: () => state.count--}),
+  ])
+}
+
+mount(document.getElementById('counter'), Counter)
+
+const GetStarted = {
+  view: () => button(
+    'Get Started',
+    {onclick, class: 'cta'},
+  ),
+}
+
+function onclick() {
+  window.location.href = 'https://github.com/tentjs/tent';
+}
+
+mount(
+  document.getElementById('get-started'),
+  GetStarted,
+);
